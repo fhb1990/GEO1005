4ca60211a79f8d4df99c0afb67c0e6b80a277fa6
fhb1990 <34007375+fhb1990@users.noreply.github.com>
2017-12-13 14:12:48 +0100

    changes 13-12

--- SpatialDecision/resources.py
+++ SpatialDecision/resources.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.6)
+# Created: di 5. dec 17:13:18 2017
+#      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
