92009c144294889faedd41ebb52ad4fcf26e8b04
gabichesurf <33785121+gabichesurf@users.noreply.github.com>
2018-01-03 16:22:37 +0100

    buffer button

--- Evacu8/Evacu8/Evacu8_dockwidget.py
+++ Evacu8/Evacu8/Evacu8_dockwidget.py
@@ -51,15 +51,35 @@ class Evacu8DockWidget(QtGui.QDockWidget, FORM_CLASS):
 
         #define globals
         self.iface = iface
+        self.canvas = self.iface.mapCanvas()
 
         # set up GUI operation signals
         # data
+        self.iface.projectRead.connect(self.updateLayers)
+        self.iface.newProjectCreated.connect(self.updateLayers)
+        self.iface.legendInterface().itemRemoved.connect(self.updateLayers)
+        self.iface.legendInterface().itemAdded.connect(self.updateLayers)
         self.load_scen.clicked.connect(self.openScenario)
         self.set_pt.clicked.connect(self.AttackPoint)
+        self.set_rad.clicked.connect(self.calculateBuffer)
+        self.selectLayerCombo.activated.connect(self.setSelectedLayer)
+        self.selectAttributeCombo.activated.connect(self.setSelectedAttribute)
 
+
+    def closeEvent(self, event):
+        # disconnect interface signals
+        try:
+            self.iface.projectRead.disconnect(self.updateLayers)
+            self.iface.newProjectCreated.disconnect(self.updateLayers)
+            self.iface.legendInterface().itemRemoved.disconnect(self.updateLayers)
+            self.iface.legendInterface().itemAdded.disconnect(self.updateLayers)
+        except:
+            pass
+
+        self.closingPlugin.emit()
+        event.accept()
 
     ##Functions##
-    #Data Functions#
     #Open Scenario
     def openScenario(self,filename=""):
         scenario_open = False
@@ -74,7 +94,52 @@ class Evacu8DockWidget(QtGui.QDockWidget, FORM_CLASS):
             if new_file:
                 self.iface.addProject(unicode(new_file))
                 scenario_open = True
+        if scenario_open:
+            self.updateLayers()
 
+    # Data Functions#
+    def updateLayers(self):
+        layers = uf.getLegendLayers(self.iface, 'all', 'all')
+        self.selectLayerCombo.clear()
+        if layers:
+            layer_names = uf.getLayersListNames(layers)
+            self.selectLayerCombo.addItems(layer_names)
+            self.setSelectedLayer()
+        else:
+            self.selectAttributeCombo.clear()
+            self.clearChart()
+
+
+    def setSelectedLayer(self):
+        layer_name = self.selectLayerCombo.currentText()
+        layer = uf.getLegendLayerByName(self.iface,layer_name)
+        self.updateAttributes(layer)
+
+    def getSelectedLayer(self):
+        layer_name = self.selectLayerCombo.currentText()
+        layer = uf.getLegendLayerByName(self.iface,layer_name)
+        return layer
+
+    def updateAttributes(self, layer):
+        self.selectAttributeCombo.clear()
+        if layer:
+            self.clearReport()
+            self.clearChart()
+            fields = uf.getFieldNames(layer)
+            if fields:
+                self.selectAttributeCombo.addItems(fields)
+                self.setSelectedAttribute()
+                # send list to the report list window
+                self.updateReport(fields)
+
+
+    def setSelectedAttribute(self):
+        field_name = self.selectAttributeCombo.currentText()
+        self.updateAttribute.emit(field_name)
+
+    def getSelectedAttribute(self):
+        field_name = self.selectAttributeCombo.currentText()
+        return field_name
 
     def AttackPoint(self, event):
         # Specify the geometry type
@@ -95,7 +160,46 @@ class Evacu8DockWidget(QtGui.QDockWidget, FORM_CLASS):
         # Add the layer to the Layers panel
         QgsMapLayerRegistry.instance().addMapLayers([layer])
 
+    # buffer functions
+    def getBufferCutoff(self):
+        buffer = self.buff_area.text()
+        if uf.isNumeric(buffer):
+            return uf.convertNumeric(buffer)
+        else:
+            return 0
 
-    def closeEvent(self, event):
-        self.closingPlugin.emit()
-        event.accept()
+    def calculateBuffer(self):
+        origins = self.getSelectedLayer().selectedFeatures()
+        layer = self.getSelectedLayer()
+        if origins > 0:
+            cutoff_distance = self.getBufferCutoff()
+            buffers = {}
+            for point in origins:
+                geom = point.geometry()
+                buffers[point.id()] = geom.buffer(cutoff_distance,12).asPolygon()
+            # store the buffer results in temporary layer called "Buffers"
+            buffer_layer = uf.getLegendLayerByName(self.iface, "Buffers")
+            # create one if it doesn't exist
+            if not buffer_layer:
+                attribs = ['id', 'distance']
+                types = [QtCore.QVariant.String, QtCore.QVariant.Double]
+                buffer_layer = uf.createTempLayer('Buffers','POLYGON',layer.crs().postgisSrid(), attribs, types)
+                uf.loadTempLayer(buffer_layer)
+                buffer_layer.setLayerName('Buffers')
+            # insert buffer polygons
+            geoms = []
+            values = []
+            for buffer in buffers.iteritems():
+                # each buffer has an id and a geometry
+                geoms.append(buffer[1])
+                # in the case of values, it expects a list of multiple values in each item - list of lists
+                values.append([buffer[0],cutoff_distance])
+            uf.insertTempFeatures(buffer_layer, geoms, values)
+            self.refreshCanvas(buffer_layer)
+
+    # after adding features to layers needs a refresh (sometimes)
+    def refreshCanvas(self, layer):
+        if self.canvas.isCachingEnabled():
+            layer.setCacheImage(None)
+        else:
+            self.canvas.refresh()
